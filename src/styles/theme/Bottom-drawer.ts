4a4ac2dcdc20f2c10cf71ed634a7a8fca49f73f7
manel <manel.hammouche@netfersmartsolutions.dz>
2024-05-14 23:01:34 +0100

    new task to push

---
+++ src/styles/theme/Bottom-drawer.ts
@@ -0,0 +1,29 @@
+
+export const header={
+display: 'flex',
+padding:'24px',
+justifyContent: 'space-between',
+alignItems: 'center',
+alignSelf: 'stretch',
+borderBottom: '1px solid #DBDBDB',
+}
+
+export const body={
+    display: 'flex',
+    
+    height: '80%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '32px',
+    flexShrink: 0
+}
+
+export const HeaderBody={
+
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    gap: '8px',
+    alignSelf: 'stretch',
+}
