f6b43fa0689413f6ddb601250558b1201587674c
manel <manel.hammouche@netfersmartsolutions.dz>
2024-05-28 12:16:00 +0100

    front company location

---
+++ src/icons/LocalizationIcon.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const LocalizationIcon = () => {
+  return (
+    <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="marker-pin-01">
+    <g id="Icon">
+    <path d="M7.00065 8.08366C7.96715 8.08366 8.75065 7.30016 8.75065 6.33366C8.75065 5.36716 7.96715 4.58366 7.00065 4.58366C6.03415 4.58366 5.25065 5.36716 5.25065 6.33366C5.25065 7.30016 6.03415 8.08366 7.00065 8.08366Z" stroke="#88909F" stroke-width="1.33" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M7.00065 13.3337C9.33398 11.0003 11.6673 8.91099 11.6673 6.33366C11.6673 3.75633 9.57798 1.66699 7.00065 1.66699C4.42332 1.66699 2.33398 3.75633 2.33398 6.33366C2.33398 8.91099 4.66732 11.0003 7.00065 13.3337Z" stroke="#88909F" stroke-width="1.33" stroke-linecap="round" stroke-linejoin="round"/>
+    </g>
+    </g>
+    </svg>
+    
+    
+    
+    
+    
+  );
+};
+
+export default LocalizationIcon;
