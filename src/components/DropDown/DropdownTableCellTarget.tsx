f766691ac286020afd4ce69a806606a28fc89ca4
BazouneChaima <bazoune.chaima@gmail.com>
2024-05-30 20:12:55 +0200

    fix conflict

--- src/components/DropDown/DropdownTableCellTarget.tsx
+++ src/components/DropDown/DropdownTableCellTarget.tsx
@@ -13,7 +13,7 @@ import { targetApis } from '@/lib/target/targetApis';
 import DeleteConfirmation from '@/components/commun/Alerts/DeleteConfirmation';
 import { DropDOwn, itemMenu } from '@/styles/theme/DropDown';
 import UpdateBottomDrawer from '@/app/dashboard/target/UpdateBottomDrawer';
-import { palette } from '@/styles/theme/colors';
+
 interface DropdownProps {
   // Function to handle modification triggered from the dropdown
   // onModify?: (data: any) => void;
@@ -24,11 +24,11 @@ interface DropdownProps {
   target: Target;
 }
 
-const Dropdown: React.FC<DropdownProps> = ({ task }) => {
+const DropdownTarget: React.FC<DropdownProps> = ({ target }) => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [isDeleteOpen, setIsDeleteOpen] = useState(false);
   const [isUpdate , setIsUpdate] = useState(false)
-  //const { targets } = useSelector((state: any) => state.target);
+  const { targets } = useSelector((state: any) => state.target);
   const dispatch = useDispatch();
   const handleOpen = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -45,39 +45,39 @@ const Dropdown: React.FC<DropdownProps> = ({ task }) => {
 
   const handleModify = React.useCallback(async (data:  Target): Promise<void> => {
 
-    // const { error, res } = await targetApis.updateTarget(data);
-    // if (error) {
-    //   return;
-    // } else {
-    //   const indexToRemove = targets.indexOf(target);
-    //   //const newTargets = targets.filter((_: any, i: any) => i !== indexToRemove);
+    const { error, res } = await targetApis.updateTarget(data);
+    if (error) {
+      return;
+    } else {
+      const indexToRemove = targets.indexOf(target);
+      //const newTargets = targets.filter((_: any, i: any) => i !== indexToRemove);
 
-    //   const newTargets =  targets.map((tar : Target) => {
-    //     if (tar.id === data.id) {
-    //       return data;
-    //     }
-    //     return tar;
-    //   });
-    //   //setIsDeleteOpen(false);
-    //   dispatch(setTargets(newTargets));
-    //   setIsUpdate(false)
-    // }
-    // handleClose();
+      const newTargets =  targets.map((tar : Target) => {
+        if (tar.id === data.id) {
+          return data;
+        }
+        return tar;
+      });
+      //setIsDeleteOpen(false);
+      dispatch(setTargets(newTargets));
+      setIsUpdate(false)
+    }
+    handleClose();
   }, []);
 
   const handleDelete = React.useCallback(async (): Promise<void> => {
 
-    // const { error, res } = await targetApis.deleteTarget(target.id);
-    // if (error) {
-    //   return;
-    // } else {
-    //   const indexToRemove = targets.indexOf(target);
-    //   const newTargets = targets.filter((_: any, i: any) => i !== indexToRemove);
-    //   setIsDeleteOpen(false);
-    //   dispatch(setTargets(newTargets));
-    // }
+    const { error, res } = await targetApis.deleteTarget(target.id);
+    if (error) {
+      return;
+    } else {
+      const indexToRemove = targets.indexOf(target);
+      const newTargets = targets.filter((_: any, i: any) => i !== indexToRemove);
+      setIsDeleteOpen(false);
+      dispatch(setTargets(newTargets));
+    }
 
-    // handleClose();
+    handleClose();
   }, []);
 
   const handleAssign = () => {
@@ -128,22 +128,10 @@ const Dropdown: React.FC<DropdownProps> = ({ task }) => {
           </MenuItem>
         </Box>
       </Menu>
-     {/* <DeleteConfirmation open={isDeleteOpen} setOpen={setIsDeleteOpen} handleDelete={handleDelete} />
-      */}
-          {isDeleteOpen &&
-        <DeleteConfirmation
-          open={isDeleteOpen}
-          setOpen={setIsDeleteOpen}
-          title="Do you want to delete this?"
-          subtitle="Are you sure you want to delete this file."
-          primary="Delete"
-          secondary="Cancel"
-          handleDelete={handleDelete} 
-          primaryColor={{ backgroundColor: palette.danger[500] }}
-        />}
-       <UpdateBottomDrawer open={isUpdate} onClose={() => setIsUpdate(!isUpdate)} onUpdateTarget={handleModify } target ={target} />
+      <DeleteConfirmation open={isDeleteOpen} setOpen={setIsDeleteOpen} handleDelete={handleDelete} />
+      <UpdateBottomDrawer open={isUpdate} onClose={() => setIsUpdate(!isUpdate)} onUpdateTarget={handleModify } target ={target} />
     </div>
   );
 };
 
-export default Dropdown;
+export default DropdownTarget;
